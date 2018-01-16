d59de8a19b63be6d1ab91a2e758253b892e44cf6
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-01-16 18:00:19 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,46 @@
 //define functions here
 
+
+function getIt(){
+  $('p').click(function(){
+    alert("Hey!");
+   
+});
+  
+  // function frameIt(){ 
+  
+    
+    function pressIt(){
+       $('#typing').on('keydown', function(key) {
+ if(key.which == 71){
+     alert('G was pressed');
+ }
+});
+
+
+function submitIt(){
+  $("form").on("submit", function() {
+ if ($( "input:first" ).val() === "correct") {
+   alert('Your form is going to be submitted now.');
+   return;
+ }
+
+});
+
+}
+
 $(document).ready(function(){
+
+
+
+ 
+
+  
+ //actions you want to happen
+});
+
 
 // call functions here
 
-});
+
+}}
